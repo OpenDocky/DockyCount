62a9c3408e4915497730ab489260e0022d493060
Drayko <no.replybot5455@gmail.com>
2026-01-10 14:29:00 +0100

    add test page

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+export const runtime = "edge"
+
+export default function TestPage() {
+    return (
+        <div style={{ padding: "50px", textAlign: "center" }}>
+            <h1>Test Page Works!</h1>
+            <p>If you see this, the Edge Runtime is working.</p>
+        </div>
+    )
+}
